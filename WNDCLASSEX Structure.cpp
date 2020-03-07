97601482d989f72244708836c2747d849c4847c5
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-03-07 15:09:48 +0900

    Update WNDCLASSEX Structure.cpp

--- WNDCLASSEX Structure.cpp
+++ WNDCLASSEX Structure.cpp
@@ -5,14 +5,14 @@
 typedef struct _WNDCLASSEX {
 	UINT		cbSize;			// 구조체 크기
 	UINT		style;			// 출력 형태
-	WNDPROC		lpfnWndProc;	// 프로시저 함수
+	WNDPROC		lpfnWndProc;		// 프로시저 함수
 	int		cbClsExtra;		// 클래스 여분 메모리
 	int		cbWndExtra;		// 윈도우 여분 메모리
 	HANDLE		hInstance;		// 윈도우 인스턴스
 	HICON		hIcon;			// 아이콘
 	HCURSOR		hCursor;		// 커서
-	HBRUSH		hbrBackground;	// 배경 색
-	LPCTSTR		lpszMenuName;	// 메뉴 이름
-	LPCTSTR		lpszClassName;	// 클래스 이름
+	HBRUSH		hbrBackground;		// 배경 색
+	LPCTSTR		lpszMenuName;		// 메뉴 이름
+	LPCTSTR		lpszClassName;		// 클래스 이름
 	HICON		hIconSm;		// 작은 아이콘
 } WNDCLASSEX;
