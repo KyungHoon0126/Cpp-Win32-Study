32516155c2c62558bf355d2ed89f8919ce78f60c
KyungHoon <kidi052812@gmail.com>
2020-06-22 08:09:36 +0900

    [ADD] : Arrangement New & Delete

--- C++/New_Delete/New_Delete/New_Delete.cpp
+++ C++/New_Delete/New_Delete/New_Delete.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#if true
+#if false
 // Use new & delete
 
 int main()
@@ -16,4 +16,35 @@ int main()
 	delete p;
 	return 0;
 }
+#endif // false
+
+#if false
+// new로 배열 할당하기
+int main()
+{
+	int arr_size;
+	
+	std::cout << "array size : ";
+	std::cin >> arr_size;
+
+	int* list = new int[arr_size];
+
+	for (int i = 0; i < arr_size; i++)
+	{
+		std::cin >> list[i];
+	}
+
+	for (int i = 0; i < arr_size; i++)
+	{
+		std::cout << i << "the element of list : " << list[i] << std::endl;
+	}
+
+	delete[] list;
+
+	return 0;
+}
+#endif // false
+
+#if true
+
 #endif // true
